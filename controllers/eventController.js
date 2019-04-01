8f4873adae111db0b7e01b1693d18559bcb88155
David <dph0718@gmail.com>
2019-04-01 00:31:57 -0600

    Removes clogs & edits createEvent

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -6,9 +6,8 @@ module.exports = {
     createEvent: (req, res) => {
         const event = {};
         for (prop in req.body) {
-            user[prop] = prop;
+            event[prop] = req.body[prop];
         };
-        console.log(event);
         db.Event.create(event);
         res.send(`Event created: ${req.body.eventName}`);
     },
@@ -16,17 +15,10 @@ module.exports = {
     // Retrieves the event by Id
     // Responds with JSON of all the Event's data
     getEvent: (req, res) => {
-        // Which event was clicked to view?
-        // grab the event id data from the user's 
-        console.log(`getEvent req.body:`);
-        console.log(req.body);;
-
         const id = req.body._id;
         db.Event
             .findById(id)
             .then(result => {
-                console.log(`Event data being delivered to client...`);
-                console.log(result);
                 res.send(result);
             });
     },
@@ -36,12 +28,10 @@ module.exports = {
         let eventName;
         db.Event
             .findOne({ _id: req.body._id }, (result) => {
-                console.log("Found sought record");
                 eventName = result.eventName;
             })
             .findOneAndRemove({ _id: req.body._id })
             .then(() => {
-                console.log(`${eventName} has been deleted.`)
                 res.send("Delete was a success.")
             })
     },
