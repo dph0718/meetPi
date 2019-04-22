67bbcb8e0eb4020f7633423de9feca440f92021d
David <dph0718@gmail.com>
2019-04-21 22:38:35 -0600

    Uses additional specified routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const apiRoutes = require("./api");
-const mapRoutes = require("./maps")
+const mapRoutes = require("./maps");
+const eventRoutes = require("./event")
+const userRoutes = require("./user")
 const router = require("express").Router();
 
-router.use('/', apiRoutes);
 router.use('/maps', mapRoutes);
+router.use('/event', eventRoutes)
+router.use('/user', userRoutes)
+
+
+// router.use('/', apiRoutes);
 
 module.exports = router;
