f1c9aa7fac96e80cbd41655f2180d4fa9cef80a5
David <dph0718@gmail.com>
2019-04-21 22:47:49 -0600

    Gets invited users' data, calculates average lat,lng coordinates, performs nearbyPlaces request, calls method for pushing Event Id into invited Users' documents

--- routes/event/index.js
+++ routes/event/index.js
@@ -8,32 +8,97 @@
 
 const router = require("express").Router();
 const fetchInvitees = require("./fetchInvitees")
+const eventControl = require("../../controllers/eventController");
+const userControl = require("../../controllers/userController");
+const axios = require("axios");
 
-
-
-// Route for creating an event:
-// [ ] Adds Invitees(by id) to the Event's user list.
-// [ ] Grabs Invitees' long and lat coords
-// [ ] and Uses them to calculate average lat/lng coords
-// [ ] stores Average lat/lng in Event database
-// [ ] 
-// [ ] 
 
 router.post('/create', (req, res) => {
 
     // let invitees = req.body.invitees;
+    // Add event Owner's coords to allCoordinates.
+    let radius = req.body.radius ? parseFloat(req.body.radius) : 1000;
+    let eventTitle = req.body.eventName ? req.body.eventName : "Unnamed Event";
 
-    let invitees = [{ userId: "5cb76b0a527d9b2eece80338" },
-    { userId: "5cb76b0a527d9b2eece80337" }
-    ]
+    // Hardcoded Dummy data from DB:
+    let invitees = [
+        { userId: "5cbbeeda6e09e51f24a44704" },
+        { userId: "5cbbeeda6e09e51f24a44705" },
+        { userId: "5cbbeeda6e09e51f24a44706" },
+    ];
 
-// Find invitees' data in User db
 
+
+    // Find invitees' data in User db
     fetchInvitees(invitees, (usersInvited) => {
 
-         
+        let numInvitees = usersInvited.length;
 
-        res.send();
+        // Store all users' coordinates in an array
+        const allCoordinates = usersInvited.map(u => {
+            return {
+                lat: u.defaultLocation.latitude,
+                lng: u.defaultLocation.longitude
+            }
+        });
+
+        // Get the sum of all Invitees' coordinates
+        let sumCoords = allCoordinates.reduce((total, val) => {
+            return { lat: total.lat + val.lat, lng: total.lng + val.lng }
+        });
+
+        // Then average the sum of coordinates
+        let avgCoords = { lat: sumCoords.lat / numInvitees, lng: sumCoords.lng / numInvitees }
+
+        // Make final Event object to be created in DB.
+        const newEvent = {
+            body: {
+                eventName: eventTitle,
+                eventZip: null,
+                eventCenter: {
+                    latitude: avgCoords.lat,
+                    longitude: avgCoords.lng
+                },
+                eventRadius: radius,
+                attendees: usersInvited.map(u => u.id),
+                // possibilities: Array,               
+            }
+
+        };
+
+        // Insert Event in DB.
+        eventControl.createEvent(newEvent, (event)=>{
+            invitees.forEach(inviT=>{
+                let addEvent = {
+                    body: {
+                        userId: inviT.userId,
+                        eventId: event._id
+                    }
+                }
+
+                userControl.updateUserEvents(addEvent)
+            })
+        })
+        // Make the nearbyPlaces request
+        axios.get(`http://localhost:3001/maps/nearby/${avgCoords.lat}/${avgCoords.lng}/${radius}`)
+            .then(result => {
+                const eventReturn = {
+                    coordinates: avgCoords,
+                    places: result.data
+                }
+                res.status(200).send(eventReturn);
+            })
+            .catch(err => {
+                console.log(`Error within axios nearby request`)
+                console.log(err);
+            });
+
+
+        // Send all the necessary data:
+        // average coordinates (to map center)
+        // places (to populate menu and generate a meetspot)
+
+
     });
 
 
